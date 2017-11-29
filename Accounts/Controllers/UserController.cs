af983e7870bba8969e2f3d68cfc70d442f72f842
Adam Sargeant <p4071825@live.tees.ac.uk>
2017-11-29 13:43:14 +0000

    adding UI project

---
+++ Accounts/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounts.Controllers
+{
+    public class UserController : Controller
+    {
+
+
+        // GET: User
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: User/Details/5
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: User/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
